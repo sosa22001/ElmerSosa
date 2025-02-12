077654198428d25da4a60dfb6f639ab3deb7a6b9
Elmer Sosa <sosa66093@gmail.com>
2025-02-12 02:00:22 -0600

    Estructuras iniciadas

--- src/interfaces/Interface.ts
+++ src/interfaces/Interface.ts
@@ -3,3 +3,18 @@ export interface SocialMediaProps {
     href: string;
     path: string;
 }
+
+export interface MainCardsProps {
+    title?: string;
+    description: string;
+}
+
+export interface Tecnologia {
+    icon:string,
+    nombre:string,
+    porcentaje:string
+}
+
+export interface Tecnologias {
+    [categoria:string] : Tecnologia[]
+}
