1811e8605aa7cd474af108d9262a45e485a0dd43
Elmer Sosa <sosa66093@gmail.com>
2025-02-18 23:53:50 -0600

    Subida lista para cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,23 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx({
+    remarkPlugins: [],
+    rehypePlugins: [],
+  })],
+  markdown: {
+    gfm: true, // Activa saltos de línea automáticos y tablas estilo GitHub,
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
     },
+  }
 });
